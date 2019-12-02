8b4ff8ff990169b8df1d6b361acda5a26875d438
Anvahi <undeadhedg@gmail.com>
2019-12-02 23:23:22 +0300

    *///*

---
+++ vplr9/vplr9/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace vplr9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.domainUpDown1 = new System.Windows.Forms.DomainUpDown();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.domainUpDown2 = new System.Windows.Forms.DomainUpDown();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(29, 24);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(326, 190);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.comboBox1);
+            this.tabPage1.Controls.Add(this.domainUpDown1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(318, 164);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Group";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(6, 32);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(185, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // domainUpDown1
+            // 
+            this.domainUpDown1.Items.Add("ФИТ");
+            this.domainUpDown1.Items.Add("УНТИ");
+            this.domainUpDown1.Items.Add("ФЭУ");
+            this.domainUpDown1.Location = new System.Drawing.Point(6, 6);
+            this.domainUpDown1.Name = "domainUpDown1";
+            this.domainUpDown1.Size = new System.Drawing.Size(185, 20);
+            this.domainUpDown1.TabIndex = 0;
+            this.domainUpDown1.SelectedItemChanged += new System.EventHandler(this.domainUpDown1_SelectedItemChanged);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.domainUpDown2);
+            this.tabPage2.Controls.Add(this.listBox1);
+            this.tabPage2.Controls.Add(this.comboBox2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(318, 164);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Subject";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // domainUpDown2
+            // 
+            this.domainUpDown2.Items.Add("ФИТ");
+            this.domainUpDown2.Items.Add("УНТИ");
+            this.domainUpDown2.Items.Add("ФЭУ");
+            this.domainUpDown2.Location = new System.Drawing.Point(6, 6);
+            this.domainUpDown2.Name = "domainUpDown2";
+            this.domainUpDown2.Size = new System.Drawing.Size(185, 20);
+            this.domainUpDown2.TabIndex = 14;
+            this.domainUpDown2.SelectedItemChanged += new System.EventHandler(this.domainUpDown2_SelectedItemChanged_1);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(6, 59);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(296, 95);
+            this.listBox1.TabIndex = 13;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(6, 32);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(185, 21);
+            this.comboBox2.TabIndex = 7;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(393, 238);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "lab9";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DomainUpDown domainUpDown1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.DomainUpDown domainUpDown2;
+    }
+}
+
---
+++ vplr9/vplr9/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace vplr9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void domainUpDown1_SelectedItemChanged(object sender, EventArgs e)
+        {
+            comboBox1.Text = "";
+            if (domainUpDown1.SelectedIndex == 0)
+            {
+                comboBox1.Items.Clear();
+                comboBox1.Items.AddRange(new string[] { "ПРИ", "ИВТ", "МОА" });
+            }
+            if (domainUpDown1.SelectedIndex == 1)
+            {
+                comboBox1.Items.Clear();
+                comboBox1.Items.AddRange(new string[] { "ТМО", "КТО", "УТС" });
+            }
+            if (domainUpDown1.SelectedIndex == 2)
+            {
+                comboBox1.Items.Clear();
+                comboBox1.Items.AddRange(new string[] { "ЭК", "ПИ", "МНТ" });
+            }
+        }
+
+        private void domainUpDown2_SelectedItemChanged_1(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            comboBox2.Text = "";
+            if (domainUpDown2.SelectedIndex == 0)
+            {
+                comboBox2.Items.Clear();
+                comboBox2.Items.AddRange(new string[] { "ПРИ", "ИВТ", "МОА" });
+            }
+            if (domainUpDown2.SelectedIndex == 1)
+            {
+                comboBox2.Items.Clear();
+                comboBox2.Items.AddRange(new string[] { "ТМО", "КТО", "УТС" });
+            }
+            if (domainUpDown2.SelectedIndex == 2)
+            {
+                comboBox2.Items.Clear();
+                comboBox2.Items.AddRange(new string[] { "ЭК", "ПИ", "МНТ" });
+            }
+        }
+
+        string[] subj;
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (domainUpDown2.SelectedIndex == 0)
+            {
+                if (comboBox2.SelectedIndex == 0)
+                {
+                    listBox1.Items.Clear();
+                    subj = new string[] { "САОД", "ООП", "Вычислительная математика", "Визуальное программирование" };
+                    listBox1.Items.AddRange(subj);
+                }
+                if (comboBox2.SelectedIndex == 1)
+                {
+                    listBox1.Items.Clear();
+                    subj = new string[] { "ООП", "Вычислительная математика" };
+                    listBox1.Items.AddRange(subj);
+                }
+                if (comboBox2.SelectedIndex == 2)
+                {
+                    listBox1.Items.Clear();
+                    subj = new string[] { "САОД", "ООП", "Вычислительная математика" };
+                    listBox1.Items.AddRange(subj);
+                }
+            }
+            if (domainUpDown2.SelectedIndex == 1)
+            {
+                if (comboBox2.SelectedIndex == 0)
+                {
+                    listBox1.Items.Clear();
+                    subj = new string[] { "Физика", "Сопромат", "Матанализ", "Философия" };
+                    listBox1.Items.AddRange(subj);
+                }
+                if (comboBox2.SelectedIndex == 1)
+                {
+                    listBox1.Items.Clear();
+                    subj = new string[] { "Матанализ", "Философия" };
+                    listBox1.Items.AddRange(subj);
+                }
+                if (comboBox2.SelectedIndex == 2)
+                {
+                    listBox1.Items.Clear();
+                    subj = new string[] { "Физика", "Сопромат", "Философия" };
+                    listBox1.Items.AddRange(subj);
+                }
+            }
+            if (domainUpDown2.SelectedIndex == 2)
+            {
+                if (comboBox2.SelectedIndex == 0)
+                {
+                    listBox1.Items.Clear();
+                    subj = new string[] { "Экономика", "Математика", "Философия" };
+                    listBox1.Items.AddRange(subj);
+                }
+                if (comboBox2.SelectedIndex == 1)
+                {
+                    listBox1.Items.Clear();
+                    subj = new string[] { "Экономика", "Математика", "Философия", "Программирование" };
+                    listBox1.Items.AddRange(subj);
+                }
+                if (comboBox2.SelectedIndex == 2)
+                {
+                    listBox1.Items.Clear();
+                    subj = new string[] { "Экономика", "Математика" };
+                    listBox1.Items.AddRange(subj);
+                }
+            }
+        }
+    }
+}
