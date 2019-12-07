71e2220e9630deb34dbf3a25c96416058cb897b6
Anvahi <undeadhedg@gmail.com>
2019-12-08 02:21:35 +0300

    unordered binary tree in saodlr6

--- saodlr6/main.cpp
+++ saodlr6/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <fstream>
-
 //Написать программу построения бинарного дерева с
 //помощью связных структур и поиска в дереве при симметричном
 //порядке обхода его.
@@ -11,118 +10,97 @@ struct node {
     node *right = nullptr;
 };
 
-struct tree{
+struct tree {
     node *root = nullptr;
+    int count = 0;
+    int number_of_elem = 0;
 };
 
-int insert (node *tr, int n){
-    int r = rand() %2; //просто подряд////метод обхода в ширину
-    if (r == 0){
-        if (tr->left == nullptr){
-            node *tmp = new node;
-            tmp->data = n;
-            tr->left = tmp;
-        }
-        else insert(tr->left, n);
-    }
-    else if (r == 1){
-        if (tr->right == nullptr){
-            node *tmp = new node;
-            tmp->data = n;
-            tr->right = tmp;
-        }
-        else insert(tr->right, n);
-    }
-}
-
-void tree_create(tree * tr, int n) {
-    if (tr->root == nullptr){
-        node *tmp = new node;
-        tr->root = tmp;
-        tr->root->data = n;
-    }
-    else insert(tr->root, n);
-}
-
-void symmetric(node * go_tr, int find){
-    if (go_tr){
+void symmetric(node *go_tr, int find) {
+    if (go_tr) {
         symmetric(go_tr->left, find);
-        std::cout << "\n|" << go_tr->data << "|";
         if (go_tr->data == find)
             std::cout << "\n|||" << go_tr->data << "|||";
+        else
+            std::cout << "\n|" << go_tr->data << "|";
         symmetric(go_tr->right, find);
     }
 }
 
-void read_file(tree* tr){
+void add(node *tr, int n) {
+    if (tr != nullptr) {
+        if (tr->left == nullptr) {
+            tr->left = new node;
+            tr->left->data = n;
+            tr->left->left = nullptr;
+            tr->left->right = nullptr;
+        } else if (tr->right == nullptr) {
+            tr->right = new node;
+            tr->right->data = n;
+            tr->right->left = nullptr;
+            tr->right->right = nullptr;
+        }
+    }
+}
+
+node *search(node *tr, int count, int lvl) {
+    node *temp;
+    if (lvl == count - 1) {
+        if (tr->left == nullptr || tr->right == nullptr)
+            return tr;
+        else
+            return nullptr;
+    }
+    temp = search(tr->left, count, lvl + 1);
+    if (temp != nullptr)
+        return temp;
+    else
+        tr = search(tr->right, count, lvl + 1);
+    return tr;
+}
+
+void tree_create(tree *tr, int n) {
+    node *tmp = new node;
+    tr->root = tmp;
+    tr->root->data = n;
+    tr->count = 1;
+    tr->number_of_elem = 1;
+}
+
+void read_file(tree *tr) {
     int a;
     std::ifstream f("bin_tr.txt");
-    while (!f.eof()){
+    f >> a;
+    tree_create(tr, a);
+    while (!f.eof()) {
         f >> a;
-        tree_create(tr, a);
+        add(search(tr->root, tr->count, 0), a);
+        ++tr->number_of_elem;
+        for (unsigned int i = UINT32_MAX; i > 0; i >>= 1) {
+            if (!(tr->number_of_elem ^ i)) {
+                ++tr->count;
+                break;
+            }
+        }
     }
     f.close();
 }
 
-void print_tree(node *tr){ //along
-    if (tr){
-        std::cout << "| " << tr->data << " ";
-        print_tree(tr->left);
-        print_tree(tr->right);
+void print_tree(node *tr, int count) {
+    if (tr) {
+        std::cout << "| " << tr->data << "(" << count << ")";
+        print_tree(tr->left, count + 1);
+        print_tree(tr->right, count + 1);
     }
 }
 
 int main() {
     tree *bin_tree = new tree;
     read_file(bin_tree);
-    print_tree(bin_tree->root);
+    print_tree(bin_tree->root, 0);
     int n;
     std::cout << "\nEnter the number what should be find:";
     std::cin >> n;
     symmetric(bin_tree->root, n);
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//struct tree * tree_create() {
-//    tree *new_tr = new tree;
-//    new_tr->root = nullptr;
-//    return new_tr;
-//}
-
-//int insert (tree *sear_tr, int n){
-//    node *sear_no, **new_no;
-//    new_no = &sear_tr->root;
-//    sear_no = sear_tr->root;
-//    for(;;){
-//        if (sear_no == nullptr){
-//            sear_no = *new_no = new node;
-//            if(sear_no != nullptr){
-//                sear_no->data = n;
-//                sear_no->left = sear_no->right = nullptr;
-//                return 1;
-//            } else
-//                return 0;
-//        } else if (n == sear_no->data) return 2;
-//        else if (n > sear_no->data){
-//            new_no = &sear_no->right;
-//            sear_no = sear_no->right;
-//        }
-//        else {
-//            new_no = &sear_no->left;
-//            sear_no = sear_no->left;
-//        }
-//    }
-//}
